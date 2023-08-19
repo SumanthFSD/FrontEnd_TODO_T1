fd11755721eb25e412fa76625427fd265c3d7fbd
Sumanth Reddy Gatla <sumanthreddy83@gmail.com>
2023-08-19 19:45:02 +0530

    Add delete Count tasks

--- models/index.js
+++ models/index.js
@@ -1,16 +1,9 @@
-   
-const task = document.querySelector("task");
-task.addEventListener("click", enableBtn) 
-let addTaskBtn = document.getElementById("add-task");
-
-function enableBtn(){
-    addTaskBtn.style.backgroundColor = "red";
-    if(task.value != ''){
-        addTaskBtn.style.display = "block";
+function showBtn(){
+    const addTaskbtn = document.getElementById("add-task");
+    if(document.getElementById('task').value != ''){
+        addTaskbtn.style.display = 'block';
+    }
 }
-}
-
-
 
 function add_task(){
     let todoList = document.getElementById("todo-list");
@@ -20,22 +13,43 @@ function add_task(){
 
     checkbox.type = "checkbox";
     checkbox.value = 1;
-    checkbox.name = "todo[]";
+    checkbox.name = "todo-checkbox";
     // create a new li element and append it to the ul with id "todo-list"
-    var input = task.value; // <-- this line
+    var input = task.value;
+    const addTaskbtn = document.getElementById("add-task");
+    addTaskbtn.style.display = 'none';
     task.value = '';
     if(input!=''){
-
         li.appendChild(checkbox);
         li.appendChild(document.createTextNode(input));
+        li.innerHTML += '<button class="delete-task" onclick = delete_task(this)><i class="fas fa-trash"></i></button>'
         todoList.appendChild(li);
-        todoList.innerHTML += '<li>' + text + ' <button onclick="delete(this);">Delete</button> </li>';
     }
     else{
         alert('Please add a task, adding a task increases chances of completing it!!!');
     }
+    updateTasksCount();
+    
 }
 
-function delete_task(task){
-    task.parentNode.parentNode.removeChild(task.parentNode);
+function delete_task(listItem){
+    listItem.parentNode.remove();
+    updateTasksCount();
 }
+
+
+function updateTasksCount(){
+    let todoList = document.getElementById("todo-list");
+    const totalCountSpan = document.getElementById('totalCount');
+    const totalItems = todoList.children.length;
+    totalCountSpan.textContent = totalItems;
+}
+
+function showCompletedTasks(){
+
+}
+
+function showUncompletedTasks(){
+
+}
+
